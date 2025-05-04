c916605a64a1ef8800aa7f0ad67a44e5a05bca69
mizAki <131885589+gonzalomz1@users.noreply.github.com>
2025-05-04 11:47:27 -0300

    cambio en codigo menucamera

--- Proyecto Inimputable/Assets/_Scripts/MainMenu/Menu/Camera/MenuCamera.cs
+++ Proyecto Inimputable/Assets/_Scripts/MainMenu/Menu/Camera/MenuCamera.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class MenuCamera : MonoBehaviour
 {
   public AnimationClip mainMenuClip;
+  public AnimationClip CreditsClip;
   public Animation animationComponent;
   public MenuManager menuManager;
 
